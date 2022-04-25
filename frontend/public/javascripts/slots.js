ef90860f9c3fecca2848f475694e35ed560dc6d9
Melodic22 <58524577+Melodic22@users.noreply.github.com>
2022-04-26 00:31:05 +0100

    added email confirmation when reserved slot is booked

--- frontend/public/javascripts/slots.js
+++ frontend/public/javascripts/slots.js
@@ -1,5 +1,4 @@
-
-//loop through availableSlots
+//loop through availableS lots
 
 
 //data will already be ordered by staff_id
@@ -30,6 +29,48 @@ const tableBody = document.getElementById('js-table-body');
 
 showSlots(slots);
 
+function showConfirmedSlotInformation(slot) {
+    alert(`Your meeting has been confirmed. \
+            \nAn email has been sent to your email address at ${email} \
+            \nand ${slot.firstname}'s email at ${slot.email}`);
+
+}
+
+function showCancelledSlotInformation(slot) {
+    alert(`Your meeting has been cancelled.`);
+}
+
+function confirmSlot(slot) {
+    //send POST request to backend
+    fetch('/slots/confirm-slot', {
+        method: 'POST',
+        headers: {
+            'Accept' : 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(slot)
+    }).then(response => response.json())
+    .then(data => {
+        console.log('Sucess:', data)
+    })
+    .catch((error) => {
+        console.log('Error:', error);
+    });
+}
+
+function confirmSlotBooking(slot) {
+
+    if (confirm(`Please confirm your booking \nfrom ${slot.time_start} to ${slot.time_finish} \
+                \nwith ${slot.firstname} ${slot.lastname} \
+                \n(${slot.email})`)) {
+        confirmSlot(slot); //only call ShowConfirmedSlotInformation if POST request successful (no error 404)
+        showConfirmedSlotInformation(slot);
+    } else {
+        showCancelledSlotInformation(slot);
+    }
+}
+
+
 function showSlots(slots) {
 
     seenStaff = [];
@@ -44,6 +85,9 @@ function showSlots(slots) {
             slotButton.classList.add('slotButton');
             slotButton.innerText = `${slot.time_start} to ${slot.time_finish}`;
             currentData.appendChild(slotButton);
+            slotButton.addEventListener('click', () => confirmSlotBooking(slot));
+
+            //daySquare.addEventListener('click', () => openExpandedEventsModal(dayString)); //used for personal events stored in database
 
             //currentData.innerText = currentData.innerText + `, ${slot.time_start} to ${slot.time_finish}`;
         } else {
@@ -61,8 +105,9 @@ function showSlots(slots) {
             slotButton.classList.add('slotButton');
             slotButton.id = `js-slot-button-no-${slot.staff_id}`;
             data.appendChild(slotButton);
+            slotButton.addEventListener('click', () => confirmSlotBooking(slot));
 
-            head.innerText = `${slot.firstname}\n${slot.email}`;
+            head.innerText = `${slot.firstname} ${slot.lastname}\n${slot.email}`;
             // data.innerText = `${slot.time_start} to ${slot.time_finish}`;
             slotButton.innerText = `${slot.time_start} to ${slot.time_finish}`;
             //add staff_id to seen staff array
